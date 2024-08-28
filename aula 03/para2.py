c77b4e30b9e90de936321823059559fc714dc69e
NICOLAS SANTOS <nicolassbrf@gmail.com>
2024-08-28 18:56:22 -0300

    laço de repetição

---
+++ aula 03/para2.py
@@ -0,0 +1,20 @@
+alunos = [
+    "nicolas",
+    "william",
+    "kaio",
+    "bruno",
+    "fernando",
+    "rafael",
+    "vitoria",
+    "leandro",
+    "lucas",
+    "luana"
+]
+alunos.sort()
+alunos.append("vitoria")
+alunos.reverse()
+y = 1
+print(y)
+for x in alunos:
+    print(y,"-",x)
+    y = y+1 
