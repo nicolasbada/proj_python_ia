a0e436c3d6d7bc71f2f853e0a0d776dbda08c704
NICOLAS SANTOS <nicolassbrf@gmail.com>
2024-08-28 17:41:55 -0300

    aula de revisão

---
+++ revisões/revisao 2.py
@@ -0,0 +1,5 @@
+idade = 16
+if(idade>=18):
+    print("maior de idade")
+else:
+    print("menor de idade")
---
+++ revisões/revisao 3.py
@@ -0,0 +1,8 @@
+idade = 16
+if(idade>18):
+    print("Adulto")
+else:
+    if(idade>13):
+        print("Adolecente")
+    else:
+        print("Criança")
